31506835fd8de2703268646fa9ba673cde98928e
Wil-TecH-rgb <justicewilson369@gmail.com>
2026-05-30 19:04:10 +0000

    Assignment submission

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
 
-def main():
-  pass #code goes here
+# File: initials.py
+# Description:  Print out my initials.
+# Assignment Number: 1
+#
+# Name: Justice Worlanyo Ekar
+# STUDENT ID:  2425404170
+# Email: 2425404170@live.gctu.edu.gh
+# Grader: Augustus 
+#
+# On my honor, Justice Worlanyo Ekar, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+def main ():
+    # printing the initials JWE as large block letters with 12 x 10 dimensions
 
+    print ()
+    print ("...JWE")
+    print ()
+    print ("...JJJJJJJJJJJJ........WWW..WW..WWW........EEEEEEEEEEEE.....") 
+    print ("...JJJJJJJJJJJJ........WWW..WW..WWW........EEEEEEEEEEEE.....") 
+    print ("...........JJJJ........WWW..WW..WWW........EEEE.............")
+    print ("...........JJJJ........WWW.WWWW.WWW........EEEE.............")
+    print ("...........JJJJ........WWW.WWWW.WWW........EEEEEEEEEEEE.....")
+    print ("...........JJJJ........WWWWW..WWWWW........EEEE.............")
+    print ("...JJJ.....JJJJ........WWWWW..WWWWW........EEEE.............")
+    print ("...JJJJJJJJJJJ....**...WWWW....WWWW...**...EEEEEEEEEEEE...**") 
+    print (".....JJJJJJJJ.....**...WWW......WWW...**...EEEEEEEEEEEE...**") 
+    print ()
 
 main()
